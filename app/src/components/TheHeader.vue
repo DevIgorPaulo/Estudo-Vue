df583e78ab52cfa8e5ec788a3499fc12f075aa55
Igor Francisco Antunes Paulo <115815356+DevIgorPaulo@users.noreply.github.com>
2025-03-06 20:17:53 -0300

    commit

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="header">Header</header>
+</template>
+
+<script>
+export default{
+    name: 'TheHeader'
+}
+</script>
+
+<style>
+    .header{
+        background-color: black;
+        color: white;
+        font-size: 20px;
+        font-weight: bold;
+    }
+</style>
